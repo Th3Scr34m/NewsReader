4381e2ea264b0dd707985ce57e908ef1775323eb
Babics Bálint <bb3enator@gmail.com>
2015-07-22 17:00:19 +0200

    Added features:
    -  DetailsLayoutAdapter

--- app/src/main/java/hu/bba/myfirstapp/adapters/DetailsLayoutAdapter.java
+++ app/src/main/java/hu/bba/myfirstapp/adapters/DetailsLayoutAdapter.java
@@ -1,28 +1,50 @@
 package hu.bba.myfirstapp.adapters;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import java.util.List;
+
+import hu.bba.myfirstapp.models.News;
+
 public class DetailsLayoutAdapter extends BaseAdapter {
+
+    private static LayoutInflater inflater = null;
+    private List<News> details;
+
+    public DetailsLayoutAdapter() {
+        super();
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        if (details == null)
+            return 0;
+        return details.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        if (details == null)
+            return null;
+        return details.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewholder;
+
         return null;
+    }
+
+    public static class ViewHolder {
+
     }
 }
